e4bc0ddbee486932108ac2c3684d52c4ee5e372b
Zak Goneau <118993110+Zak-Goneau@users.noreply.github.com>
2023-01-13 18:57:55 -0500

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,4 @@
+// When button A is pressed it will be shocked.
 input.onButtonPressed(Button.A, function () {
     for (let index = 0; index < 1; index++) {
         basic.showIcon(IconNames.Surprised)
@@ -5,13 +6,16 @@ input.onButtonPressed(Button.A, function () {
         basic.showIcon(IconNames.Happy)
     }
 })
+// When button A + B is pressed it will intorduce itself.
 input.onButtonPressed(Button.AB, function () {
     basic.showString("Hi, I'm Neilson!")
     basic.showIcon(IconNames.Happy)
 })
+// When button B is pressed it will sleep.
 input.onButtonPressed(Button.B, function () {
     basic.showIcon(IconNames.Asleep)
 })
+// When shaken it will become angry.
 input.onGesture(Gesture.Shake, function () {
     for (let index = 0; index < 1; index++) {
         basic.showIcon(IconNames.Angry)
@@ -19,4 +23,5 @@ input.onGesture(Gesture.Shake, function () {
         basic.showIcon(IconNames.Happy)
     }
 })
+// Starts with happy face.
 basic.showIcon(IconNames.Happy)
